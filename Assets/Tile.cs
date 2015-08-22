0df065e0485bc2dc1a78908126e6f43aa0cfe865
Aldo <aldocurtis@gmail.com>
2015-08-22 12:46:05 +0100

    Started grid based movement, added AldoScene

---
+++ Assets/Tile.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	
+	public GameObject occupant;
+	public string effectsOnTile;
+
+	public Vector3 characterPosition;
+	
+	public Tile up;
+	public Tile right;
+	public Tile left; 
+	public Tile down;
+
+	// Use this for initialization
+	void Start () {
+		characterPosition = this.transform.position + new Vector3 (0, 1, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		if (right) {
+			if(right.left == this)
+			{
+			Debug.DrawLine(this.transform.position,
+			               right.transform.position,Color.blue);
+			}
+			else{
+				Debug.DrawLine(this.transform.position,
+				               right.transform.position,Color.red);
+			}
+		}
+		if (down) {
+			if(down.up == this)
+			{
+			Debug.DrawLine(this.transform.position,
+			               down.transform.position,Color.blue);
+			}
+			else{
+				Debug.DrawLine(this.transform.position,
+				               down.transform.position,Color.red);
+			}
+		}
+	}
+}
