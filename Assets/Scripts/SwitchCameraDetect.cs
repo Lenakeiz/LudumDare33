ba5d99f4da7d4ed9d0196e22f04363aa0798d812
Lenakeiz <andrea.castegnaro@outlook.com>
2015-08-23 22:33:09 +0100

    Added second camera on UI plus switching logic on LevelGame Controller

---
+++ Assets/Scripts/SwitchCameraDetect.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchCameraDetect : MonoBehaviour {
+
+	public int RoomIndex;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			GameObject.Find("Camera").GetComponent<LevelController>().ChangeMiniCamera(RoomIndex);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
