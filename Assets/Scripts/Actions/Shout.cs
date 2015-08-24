2964bca9ea127d66a59a009581f7f88cf8da97b3
Aldo <aldocurtis@gmail.com>
2015-08-24 10:39:43 +0100

    Started shout ability

---
+++ Assets/Scripts/Actions/Shout.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shout : MonoBehaviour {
+
+
+	public AudioClip shoutClip;
+	
+	
+	
+	public Vector3 hauntDirection;
+	
+	public float arrowOffset = 2.0f;
+	
+	public float hauntDuration;
+	float hauntDurationTimer;
+	
+	public float hauntCooldown;
+	public float hauntCooldownTimer;
+	
+	public float amountOfFear;
+	
+	Tile[] hauntedTiles = new Tile[2];
+	
+	int lockNumber;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
